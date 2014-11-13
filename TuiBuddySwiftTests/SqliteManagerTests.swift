a52d4b55609abc05ef86502953b3e42e453d2680
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-11-13 12:34:17 +0100

    completed SqliteManager unit tests

--- TuiBuddySwiftTests/SqliteManagerTests.swift
+++ TuiBuddySwiftTests/SqliteManagerTests.swift
@@ -22,17 +22,28 @@ class SqliteManagerTests: XCTestCase {
         XCTAssertGreaterThan(countElements(attractions), 0, "did not read attractions from the database")
     }
     
-    func testWriteDeleteAttractions() {
+    func testWriteUpdateDeleteAttractions() {
         var initialCount = countElements(attractions)
+        // create attraction
         var attraction = Attraction()
         attraction.name = "really_weird_test_attraction_name"
+        // write in DB
         SqliteManager.sharedInstance.writeAttraction(attraction)
         attractions = SqliteManager.sharedInstance.readAttractions()
         XCTAssertEqual(countElements(attractions), initialCount + 1, "did not write the attraction properly")
         // the inserted attraction should be the last
-        let retrievedAttraction = attractions.last!
+        var retrievedAttraction = attractions.last!
         XCTAssertEqual(retrievedAttraction.name, attraction.name, "did not recover the attraction properly")
-        SqliteManager.sharedInstance.deleteAttraction(retrievedAttraction)
+        // change and update attraction
+        retrievedAttraction.name = "yet_another_really_weird_test_attraction_name"
+        SqliteManager.sharedInstance.updateAttraction(retrievedAttraction)
+        attractions = SqliteManager.sharedInstance.readAttractions()
+        XCTAssertEqual(countElements(attractions), initialCount + 1, "updating changed the number of attractions in the db")
+        // id shouldn't have changed
+        let updatedAttraction = attractions.filter({$0.id == retrievedAttraction.id})[0]
+        XCTAssertEqual(updatedAttraction.name, retrievedAttraction.name, "did not update the attraction correctly")
+        // delete attraction
+        SqliteManager.sharedInstance.deleteAttraction(updatedAttraction)
         attractions = SqliteManager.sharedInstance.readAttractions()
         XCTAssertEqual(countElements(attractions), initialCount, "did not delete the attraction properly")
     }
