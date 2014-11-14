08058dc412cfdde153f06b282ffa6530cbe7cc12
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-11-14 10:45:09 +0100

    completed NotificationManager tests

--- TuiBuddySwift/NotificationManager.swift
+++ TuiBuddySwift/NotificationManager.swift
@@ -16,13 +16,21 @@ class NotificationManager {
     }
     
     private class var _notifications: Dictionary<Int32, Bool> {
-        get {return _private_struct.notifications}
-        set {_private_struct.notifications = newValue}
+        get {
+            return _private_struct.notifications
+        }
+        set {
+            _private_struct.notifications = newValue
+        }
     }
     
     public class var _scheduleLocalNotification: (UILocalNotification) -> Void {
-        get {return _private_struct.scheduleLocalNotification}
-        set {_private_struct.scheduleLocalNotification = newValue}
+        get {
+            return _private_struct.scheduleLocalNotification
+        }
+        set {
+            _private_struct.scheduleLocalNotification = newValue
+        }
     }
     
     class func resetNotifications() {
@@ -31,12 +39,15 @@ class NotificationManager {
     
     class func sendLocalNotificationForAttraction(attraction: Attraction, withMessage message: String) {
         // send local notification if not sent
-        if _notifications[attraction.id] != nil && !_notifications[attraction.id]! {
+        if _notifications[attraction.id] == nil || _notifications[attraction.id]! == false {
             var localNotification = UILocalNotification()
             localNotification.timeZone = NSTimeZone.defaultTimeZone()
             localNotification.alertBody = message
             localNotification.userInfo = ["name":attraction.name,
                 "url":attraction.url]
+            // set the attraction as notified
+            _notifications[attraction.id] = true
+            // schedule notification
             _scheduleLocalNotification(localNotification)
         }
     }
--- TuiBuddySwiftTests/NotificationManagerTests.swift
+++ TuiBuddySwiftTests/NotificationManagerTests.swift
@@ -14,15 +14,22 @@ class NotificationManagerTests: XCTestCase {
     var shouldSendLocalNotification = false
     var attractionInNotification: Attraction? = nil
     var messageInNotification: String? = nil
+    var notificationsReceived: Dictionary<Int32, Int>? = nil
+    
+    func handleLocalNotification (notification: UILocalNotification) {
+        XCTAssertTrue(self.shouldSendLocalNotification, "sent local notification when it shouldn't")
+        
+        XCTAssertEqual(notification.alertBody!, messageInNotification!, "wrong message sent in notification")
+        XCTAssertEqual(notification.userInfo!["name"] as String, attractionInNotification!.name, "wrong name sent in notification")
+        XCTAssertEqual(notification.userInfo!["url"] as String, attractionInNotification!.url, "wrong url sent in notification")
+        
+        notificationsReceived![attractionInNotification!.id]!++
+    }
 
     override func setUp() {
         super.setUp()
         // Mock scheduleLocalNotification
-        NotificationManager._scheduleLocalNotification = {[unowned self](notification: UILocalNotification) -> Void in
-            XCTAssertTrue(self.shouldSendLocalNotification, "sent local notification when it shouldn't")
-            XCTAssertEqual(notification.alertBody!, self.messageInNotification!, "wrong message sent in notification")
-            XCTAssertEqual(notification.userInfo!["name"] as String, self.attractionInNotification!.name, "wrong name sent in notification")
-            XCTAssertEqual(notification.userInfo!["url"] as String, self.attractionInNotification!.url, "wrong url sent in notification")}
+        NotificationManager._scheduleLocalNotification = handleLocalNotification
         
         NotificationManager.resetNotifications()
     }
@@ -35,15 +42,50 @@ class NotificationManagerTests: XCTestCase {
 
     func testBlockNotifications() {
         // test that the NotificationManager blocks notifications for the same attraction until reset
+        // init attractions
         var attraction1 = Attraction()
         attraction1.name = "name1"
         attraction1.url = "url1"
         var attraction2 = Attraction()
+        attraction2.id = -2
         attraction2.name = "name2"
         attraction2.url = "url2"
+        // function to check the number of notifications received
+        func attractionsShouldHaveReceived(notifications1: Int, notifications2: Int) {
+            XCTAssertEqual(notificationsReceived![attraction1.id]!, notifications1, "wrong number of notifications received for attraction 1")
+            XCTAssertEqual(notificationsReceived![attraction2.id]!, notifications2, "wrong number of notifications received for attraction 2")
+        }
+        // init notifications received
+        notificationsReceived = [attraction1.id: 0, attraction2.id: 0]
         // send notification for attraction1
+        attractionInNotification = attraction1
         shouldSendLocalNotification = true
         messageInNotification = "message1"
         NotificationManager.sendLocalNotificationForAttraction(attraction1, withMessage: messageInNotification!)
+        attractionsShouldHaveReceived(1, 0)
+        // send a hundred more notifications for attraction1
+        shouldSendLocalNotification = false
+        for i in 1...100 {
+            NotificationManager.sendLocalNotificationForAttraction(attraction1, withMessage: messageInNotification!)
+        }
+        attractionsShouldHaveReceived(1, 0)
+        // send notification for attraction2
+        attractionInNotification = attraction2
+        shouldSendLocalNotification = true
+        messageInNotification = "message2"
+        NotificationManager.sendLocalNotificationForAttraction(attraction2, withMessage: messageInNotification!)
+        attractionsShouldHaveReceived(1, 1)
+        // send a hundred more notifications for attraction2
+        shouldSendLocalNotification = false
+        for i in 1...100 {
+            NotificationManager.sendLocalNotificationForAttraction(attraction2, withMessage: messageInNotification!)
+        }
+        attractionsShouldHaveReceived(1, 1)
+        // reset notifications (unlock the phone)
+        NotificationManager.resetNotifications()
+        // send notification2 again
+        shouldSendLocalNotification = true
+        NotificationManager.sendLocalNotificationForAttraction(attraction2, withMessage: messageInNotification!)
+        attractionsShouldHaveReceived(1, 2)
     }
 }
